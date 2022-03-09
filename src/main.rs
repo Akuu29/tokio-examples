69566742dc7755cecd0bb613d45b2e8b513d5dab
Akuu29 <owner.akiu0529@gmail.com>
2022-03-09 21:59:32 +0900

    update update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
-fn main() {
-    println!("Hello, world!");
+async fn greet() {
+    println!("hello");
 }
+
+#[tokio::main]
+async fn main() {
+    // この時点ではgreet()の中身は実行されない。
+    let greeting = greet();
+
+    // 先に実行される。
+    println!("world!");
+
+    // greetingにたいして.awaitを呼び出すことでgreet()の中身が実行される
+    greeting.await;
+}
+
+// #[tokio::main]
+// async fn main() {
+//     println!("hello");
+// }
+//         |      |
+//         |      |
+//         |      |
+//         |      |
+//    -----        -----
+//     \              /
+//      \            /     #[tokio::main]マクロをmain関数につけることで 
+//       \          /      async fn main() → fn main()へと変換される。
+//        \        /
+//         \      /
+//          \    /
+//           \  /
+//            \/
+//
+// fn main() {
+//     let mut rt = tokio::runtime::Runtime::new().unwrap();
+//     rt.block_on(async {
+//         println!("hello");
+//     })
+// }
